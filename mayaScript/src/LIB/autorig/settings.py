821fe710dd47a3f49562d14300eaa11a53b2695c
loichuss <loic.huss@gmail.com>
2012-11-07 00:24:57 +0100

    [CREATE] first commit

---
+++ mayaScript/src/LIB/autorig/settings.py
@@ -0,0 +1,45 @@
+#
+#   Script create by Loic Huss
+#   Version 1.0
+#   Last Modification 7 nov 2012
+#
+#   Global variable and definition for the autorig
+#
+
+import autorig.tools.hierarchy as arHierarchy
+
+
+
+
+#              #
+#   VARIABLE   #
+#              #
+
+RIG   = 'RIG'
+TPL   = 'TPL'
+TSK   = '2SK'
+SKN   = 'SKN'
+BONES = {
+         RIG:[],
+         TPL:[],
+         TSK:[],
+         }
+
+
+
+#              #
+#  DEFINITION  #
+#              #
+
+def decorator(attempt):
+    def wrapper(func):
+        def wrapped(arg):
+            # create hierarchy group
+            hierarchy = arHierarchy.createHierarchy()
+
+            result = func(arg, hierarchy)
+            return result   
+        return wrapped
+    return wrapper
+
+
