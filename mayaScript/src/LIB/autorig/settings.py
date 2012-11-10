b0886a8554a619c66decfedc753eee166b3a1b20
loichuss <loic.huss@gmail.com>
2012-11-11 00:57:49 +0100

    [CHANGE] decorator automaticaly creating sets

--- mayaScript/src/LIB/autorig/settings.py
+++ mayaScript/src/LIB/autorig/settings.py
@@ -6,8 +6,9 @@
 #   Global variable and definition for the autorig
 #
 
-import autorig.tools.hierarchy as arHierarchy
 
+import pymel.core as pmc
+import autorig.tools.hierarchy as arHierarchy
 
 
 
@@ -15,15 +16,11 @@ import autorig.tools.hierarchy as arHierarchy
 #   VARIABLE   #
 #              #
 
-RIG      = 'RIG'
-TPL      = 'TPL'
-TSK      = '2SK'
-SKN      = 'SKN'
 TPL_NAME = '_TPLjnt'
 BONES    = {
-            RIG:[],
-            TPL:[],
-            TSK:[],
+            'RIG':{},
+            'TPL':[],
+            '2SK':[],
             }
 
 
@@ -32,13 +29,43 @@ BONES    = {
 #  DEFINITION  #
 #              #
 
-def decorator(attempt):
+def completion():
     def wrapper(func):
         def wrapped(arg):
+            
             # create hierarchy group
             hierarchy = arHierarchy.createHierarchy()
-
-            result = func(arg, hierarchy)
+            
+            _work = {
+                     'CONTROLS': {},
+                     'PICK':     {},
+                     }
+            
+            result = func(arg, hierarchy, _work)
+            
+            
+            #              #
+            #  Create Set  #
+            #              #
+            
+            # unselect everything
+            pmc.select(clear=True)
+            
+            # loop per set
+            for elts in sorted(_work['CONTROLS']):
+                
+                # create and fill the set
+                elt = elts.split('|')
+                set = pmc.sets(name=elt[-1])
+                pmc.sets(set, addElement=_work['CONTROLS'][elts])
+                
+                # parent the set
+                if len(elt)==1:
+                    pmc.sets(hierarchy['CONTROLS'], addElement=set)
+                else:
+                    pmc.sets(elt[-2], addElement=set)
+                
+            
             return result   
         return wrapped
     return wrapper
